929f910ab40b1d82ef3b8bf02be55bab9733b610
antero9471 <anteros.dev9471@gmail.com>
2022-05-14 03:25:54 +0900

    completed search button

--- pages/view.tsx
+++ pages/view.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
 import React, { useState } from "react"
+import SearchInput from '../components/SearchInput'
 import Link from 'next/link'
 
 const View: NextPage = () => {
   const [ownerKey, setOwnerKey] = useState("");
+
   return (
-    <div className='container'>
-      View
+    <div className='container p-12'>
+      <div className='flex flex-row'>
+        <span className='text-4xl mr-10 font-bold'> NFT Viewer </span>
+        <SearchInput className='flex flex-row w-1/2 items-center justify-center' text={ownerKey} onChangeHandler={setOwnerKey}></SearchInput>
+      </div>
     </div>
   )
 }
