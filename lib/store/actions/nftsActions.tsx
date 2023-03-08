cf8e0d1b834bec010cbfe479af5f05dfb5d2cb26
antero9471 <anteros.dev9471@gmail.com>
2023-03-08 19:38:10 +0200

    udpate

--- lib/store/actions/nftsActions.tsx
+++ lib/store/actions/nftsActions.tsx
@@ -1,15 +1,19 @@
 import { Connection, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { getParsedNftAccountsByOwner, isValidSolanaAddress, createConnectionConfig, } from "@nfteyez/sol-rayz";
+import {
+  getParsedNftAccountsByOwner,
+  isValidSolanaAddress,
+  createConnectionConfig,
+} from "@nfteyez/sol-rayz";
 
 export const getAllNftData = async (owner_key: string) => {
   const connect = createConnectionConfig(clusterApiUrl("devnet"));
   let ownerToken = owner_key;
   const result = isValidSolanaAddress(ownerToken);
   console.log("result", result);
-  const nfts = await getParsedNftAccountsByOwner({
+  const nfts: any = await getParsedNftAccountsByOwner({
     publicAddress: ownerToken,
     connection: connect,
     serialization: true,
   });
   return nfts;
-}
+};
